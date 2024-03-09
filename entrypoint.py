d5d5617663b90d0d34d0373de2532c69781a4e46
Marc Khoury <khoury.marc@gmail.com>
2024-03-08 22:37:02 -0500

    Refactor format_review_comment function to improve code readability and HTML formatting

--- entrypoint.py
+++ entrypoint.py
@@ -120,17 +120,18 @@ def get_review(
 
 def format_review_comment(summarized_review: str, chunked_reviews: List[str]) -> str:
     """Format reviews"""
+    s = summarized_review.replace('\n', '<br>')
     if len(chunked_reviews) == 1:
-        return summarized_review
-    unioned_reviews = "\n".join(chunked_reviews)
+        return s
+    unioned_reviews = "<br>".join(chunked_reviews)
     review = f"""
 <h2>Summary</h2>
 
-{summarized_review.replace('\n', '<br>')}
+{s}
 
 <hr>
 <h2>Chunks</h2>
-{unioned_reviews.replace('\n', '<br>')}
+{unioned_reviews}
     """
     return review
 
